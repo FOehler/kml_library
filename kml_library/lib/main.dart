34da9de4a67380b4338c52fb09e39e380cc942f5
Fabrice Oehler <32290321+FOehler@users.noreply.github.com>
2023-09-13 19:38:31 +0200

    Wrap app in ProviderScope

--- kml_library/lib/main.dart
+++ kml_library/lib/main.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:kml_library/src/presentation/view/collection_list_page.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
-  runApp(MainApp());
+  runApp(
+    ProviderScope(
+      child: MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
